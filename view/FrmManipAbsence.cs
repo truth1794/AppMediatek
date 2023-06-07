dd292c0851562323703fa7dea34d51357943d94b
truth1794 <76655153+truth1794@users.noreply.github.com>
2023-06-07 13:48:26 +0000

    Update FrmManipAbsence.cs

--- view/FrmManipAbsence.cs
+++ view/FrmManipAbsence.cs
@@ -1,69 +1,147 @@
-using AppMediatek.dal;
-using AppMediatek.Model;
+using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using AppMediatek.controller;
+using AppMediatek.Model;
 
-namespace AppMediatek.controller
+namespace AppMediatek.view
 {
     /// <summary>
-    /// Contrôleur de FrmGestion
+    /// Fenêtre d'affichage des développeurs et de leurs profils
     /// </summary>
-    public class FrmManipAbsController
+    public partial class FrmManipAbsence : Form
     {
         /// <summary>
-        /// objet d'accès aux opérations possible sur Absence
+        /// Booléen pour savoir si une modification est demandée
         /// </summary>
-        private readonly AbsenceAcces absenceAcces;
+        private bool modifEnCours = false;
         /// <summary>
-        /// objet d'accès aux opérations possible sur Motif
+        /// Controleur de la fenêtre
         /// </summary>
-        private readonly MotifAcces motifAcces;
+        private FrmManipAbsController controller;
 
         /// <summary>
-        /// Récupère les acces aux données
+        /// Entier contenant l'ID du personnel selectionne
         /// </summary>
-        public FrmManipAbsController()
+        private int idPersonnel;
+        /// <summary>
+        /// chaine contenant la date de debut de l'absence a changer
+        /// </summary>
+        private string dateDebutToChange;
+
+        /// <summary>
+        /// construction des composants graphiques et appel des autres initialisations
+        /// </summary>
+        /// <param name="dateDebut">date de debut d'absence</param>
+        /// <param name="dateFin">date de fin d'absence</param>
+        /// <param name="idMotif">id du motif</param>
+        /// <param name="modifEnCours">modification en cours</param>
+        /// <param name="idPersonnel">id du personnel</param>
+        public FrmManipAbsence(string dateDebut, string dateFin, int idMotif, bool modifEnCours, int idPersonnel)
         {
-            absenceAcces = new AbsenceAcces();
-            motifAcces = new MotifAcces();
+            InitializeComponent();
+            Init(dateDebut, dateFin, idMotif, modifEnCours, idPersonnel);
         }
 
         /// <summary>
-        /// Récupère et retourne les absences du personnel selectionne
+        /// Initialisations :
+        /// Création du controleur et remplissage des listes
         /// </summary>
-        /// <param name="idPerso">id du personnel</param>
-        /// <returns>liste des absences</returns>
-        public List<Absence> GetAbsences(int idPerso)
+        /// <param name="dateDebut">date de debut d'absence</param>
+        /// <param name="dateFin">date de fin d'absence</param>
+        /// <param name="idMotif">id du motif</param>
+        /// <param name="modifEnCours">modification en cours</param>
+        /// <param name="idPersonnel">id du personnel</param>
+        private void Init(string dateDebut, string dateFin, int idMotif, bool modifEnCours, int idPersonnel)
         {
-            return absenceAcces.GetAbsences(idPerso);
+            controller = new FrmManipAbsController();
+            if (modifEnCours)
+            {
+                dateTDebut.Value = DateTime.Parse(dateDebut);
+                dateTFin.Value = DateTime.Parse(dateFin);
+            }
+            else
+            {
+                dateTDebut.Value = DateTime.Today;
+                dateTFin.Value = DateTime.Today;
+            }
+            List<Motif> lesMotifs = controller.GetMotifs();
+            for (int i = 0; i < lesMotifs.Count; i++)
+            {
+                cmbMotif.Items.Add(lesMotifs[i].Nom);
+            }
+            cmbMotif.SelectedIndex = idMotif - 1;
+            this.modifEnCours = modifEnCours;
+            this.idPersonnel = idPersonnel;
+            dateDebutToChange = dateDebut;
         }
 
         /// <summary>
-        /// Récupère et retourne les motifs
+        /// Bouton de validation de la modification ou l'ajout d'une absence
         /// </summary>
-        /// <returns>liste des motifs</returns>
-        public List<Motif> GetMotifs()
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnValider_Click(object sender, EventArgs e)
         {
-            return motifAcces.GetMotifs();
+            if (cmbMotif.SelectedIndex == -1)
+            {
+                MessageBox.Show("Tous les champs doivent être remplis.", "Information");
+            }
+            else if(DateTime.Compare(dateTDebut.Value,dateTFin.Value) > 0)
+            {
+                MessageBox.Show("La date de fin est anterieure a la date de debut.", "Information");
+            }
+            else
+            {
+                DialogResult check = MessageBox.Show("Cette operation va modifier la base de donnee !", "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                if (check == DialogResult.OK)
+                {
+                    if (modifEnCours)
+                    {
+                        UpdateAbsence();
+                    }
+                    else
+                    {
+                        AddAbsence();
+                    }
+                    this.Close();
+                    this.Dispose();
+                }
+            }
         }
 
         /// <summary>
-        /// Ajoute une absence
+        /// Bouton d'annulation de la modification ou l'ajout d'une absence
         /// </summary>
-        /// <param name="absence">objet absence à ajouter</param>
-        public void AddAbsence(Absence absence)
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAnnuler_Click(object sender, EventArgs e)
         {
-            absenceAcces.AddAbsence(absence);
+            this.Close();
+            this.Dispose();
         }
 
         /// <summary>
-        /// Modification d'une absence
+        /// Appel de la fonction pour ajouter une absence
         /// </summary>
-        /// <param name="updatedAbsence">object contenant l'absence mise a jour</param>
-        /// <param name="absenceToUpdate">object contenant l'absence a mettre a jour</param>
-        public void UpdateAbsence(Absence updatedAbsence, Absence absenceToUpdate)
+        private void AddAbsence()
         {
-            absenceAcces.UpdateAbsence(updatedAbsence, absenceToUpdate);
+            controller.AddAbsence(new Absence(
+                idPersonnel,
+                dateTDebut.Value,
+                cmbMotif.SelectedIndex + 1,
+                dateTFin.Value,
+                null));
+        }
+
+        /// <summary>
+        /// Appel de la fonction pour mettre a jour une absence
+        /// </summary>
+        private void UpdateAbsence()
+        {
+            Absence updatedAbsence = new Absence(idPersonnel, dateTDebut.Value, cmbMotif.SelectedIndex + 1, dateTFin.Value, null);
+            Absence AbsenceToUpdate = new Absence(idPersonnel, DateTime.Parse(dateDebutToChange), 1, DateTime.Today, null);
+            controller.UpdateAbsence(updatedAbsence, AbsenceToUpdate);
         }
     }
 }
